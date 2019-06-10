30add4c488a855be1dd76e9c8fa1affcfb4489ce
Marcos Lacerda <markos.lacerda@gmail.com>
2019-06-09 21:30:20 -0300

    Add the button to handler change animation type

--- SwiftUI-SimpleSample.playground/Contents.swift
+++ SwiftUI-SimpleSample.playground/Contents.swift
@@ -15,32 +15,73 @@ struct LiveView: View {
   // A initial position
   @State var position: CGPoint = .zero
 
+  // Define the initial animation
+  @State var animation: Animation = .spring()
+
+  // Define the index of actual animation
+  @State var indexOfAnimation: Int = 0
+
+  // Define the name of actual animation
+  @State var actualAnimation: String = "Spring"
+
   var body: some View {
     // Define a "stackView" in center both axis
-    ZStack {
-      // Define a big circle
-      Circle()
-        .foregroundColor(Color.black.opacity(0.1))
-        .frame(width: 320, height: 320)
+    VStack {
 
-      // Define a image to centered a big circle
-      // and events to handler the move it
-      // out of the larger circle
-      Image(systemName: "asterisk.circle.fill")
-        .scaleEffect(CGSize(width: 3, height: 3))
-        .foregroundColor(Color.blue.opacity(0.5))
-        .offset(by: self.position)
-        .animation(.spring())
-        .gesture(DragGesture()
-          .onChanged {
-            self.position = $0.location
-          }
-          .onEnded { _ in
-            if sqrt(self.position.x * self.position.x + self.position.y * self.position.y) > 160 {
-              self.position = .zero
+      ZStack {
+        // Define a big circle
+        Circle()
+          .foregroundColor(Color.black.opacity(0.1))
+          .frame(width: 320, height: 320)
+
+        // Define a image to centered a big circle
+        // and events to handler the move it
+        // out of the larger circle
+        Image(systemName: "asterisk.circle.fill")
+          .scaleEffect(CGSize(width: 3, height: 3))
+          .foregroundColor(Color.blue.opacity(0.5))
+          .offset(by: self.position)
+          .animation(animation)
+          .gesture(DragGesture()
+            .onChanged {
+              self.position = $0.location
             }
-          })
+            .onEnded { _ in
+              if sqrt(self.position.x * self.position.x + self.position.y * self.position.y) > 160 {
+                self.position = .zero
+              }
+            })
       }
+
+      // Define button to handler the change animation
+      Button(action: {
+        self.indexOfAnimation += 1
+        
+        if self.indexOfAnimation > 2 {
+          self.indexOfAnimation = 0
+        }
+
+        switch self.indexOfAnimation {
+        case 0:
+          self.animation = .spring()
+          self.actualAnimation = "Spring"
+
+        case 1:
+          self.animation = .fluidSpring()
+          self.actualAnimation = "Fluid Spring"
+
+        default:
+          self.animation = .basic()
+          self.actualAnimation = "Basic"
+        }
+
+        print("Animation changed")
+      }) {
+        Text("Change Animation")
+      }.padding()
+
+      Text("Actual animation: " + actualAnimation)
+    }
   }
 
 }
